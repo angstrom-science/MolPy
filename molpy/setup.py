a8df5243f9a8499854c24fa0ad293dedd974d825
Ångström <angstrom-science@users.noreply.github.com>
2015-06-09 20:54:09 +0530

    some details added - commit.

--- molpy/setup.py
+++ molpy/setup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 __author__ = 'Prabhat Kumar'
+"""
+MolPy (pronounced "Mol Pie") is the open-source fundamental package for Molecular Properties Prediction by Python.
+Principal Architect : Prabhat Kumar
+Version             : Hydrogen
+Release             : 1.0.0
+License             : Apache
+"""
+
 # operating system interfaces
 import os
