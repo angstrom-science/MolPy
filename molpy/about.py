826d7c13db00b994152d07e95020debbdf8a2203
Ångström <angstrom-science@users.noreply.github.com>
2015-06-10 01:26:20 +0530

    about function for MolPy.🌟
    MolPy About by Prabhat Kumar.

--- molpy/about.py
+++ molpy/about.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python
+__author__ = 'Prabhat Kumar'
+
+def about():
+  """
+  [About for MolPy], gives information about it's dependencies and their version:
+    1. Principal Architect,
+    2. Author,
+    3. Copyright,
+    4. Version,
+    5. Release, and
+    6. License
+  """
+  print("////----------------------------------------////")
+  print("MolPy: Molecular Properties Prediction by Python")
+  print("Principal Architect: Prabhat Kumar")
+  print("Author: Prabhat Kumar")
+  print("Version: Hydrogen")
+  print("Release: 1.0.0")
+  print("License: Apache")
+  print("////----------------------------------------////")
+
+if __name__ == "__main__":
+    about()
